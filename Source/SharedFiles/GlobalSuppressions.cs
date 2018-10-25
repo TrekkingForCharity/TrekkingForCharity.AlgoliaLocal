da384271544a87f1a7b8b71c540465ad1dd4f0f2
Unknown <andrew@deviouscreation.com>
2018-10-25 20:48:35 +0100

    Adding stylecop
    Adding stylecop analyzers to the project and adding the reference files

---
+++ Source/SharedFiles/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Trekking for Charity. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "Initially suppressing everything before evaluating what's wanted and what's appropriate.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1117:Parameters must be on same line or separate lines", Justification = "Initially suppressing everything before evaluating what's wanted and what's appropriate.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1118:Parameter must not span multiple lines", Justification = "Initially suppressing everything before evaluating what's wanted and what's appropriate.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names must not begin with underscore", Justification = "Initially suppressing everything before evaluating what's wanted and what's appropriate.")]
