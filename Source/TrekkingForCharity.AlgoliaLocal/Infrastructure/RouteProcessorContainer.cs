f1be3b2951be2721a9f4ea4972b02d88154798ba
roly445 <andrew@deviouscreation.com>
2018-10-29 01:01:15 +0000

    Adding custom routes to the listing class and changing its name
    I have change the name of the class to better represent what it is doing.
    I have also adding the ability to introduce custom routes created outside of the project.

---
+++ Source/TrekkingForCharity.AlgoliaLocal/Infrastructure/RouteProcessorContainer.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Trekking for Charity. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ResultMonad;
+
+namespace TrekkingForCharity.AlgoliaLocal.Infrastructure
+{
+    public sealed class RouteProcessorContainer
+    {
+        private readonly List<IRouteProcessor> _customRouteProcessors;
+        private readonly List<IRouteProcessor> _routeProcessors;
+
+        public RouteProcessorContainer(IDataRepository dataRepository)
+        {
+            this._customRouteProcessors = new List<IRouteProcessor>();
+            this._routeProcessors = new List<IRouteProcessor>();
+
+            var assembly = typeof(IRouteProcessor).GetTypeInfo().Assembly;
+
+            var types = assembly.DefinedTypes.Where(x => !x.IsAbstract && x.ImplementedInterfaces.Contains(typeof(IRouteProcessor)));
+
+            foreach (var typeInfo in types)
+            {
+                this._routeProcessors.Add(
+                    (IRouteProcessor)Activator.CreateInstance(typeInfo.AsType(), dataRepository));
+            }
+        }
+
+        public IReadOnlyList<IRouteProcessor> RouteProcessors => this._routeProcessors.AsReadOnly();
+
+        public IReadOnlyList<IRouteProcessor> CustomRouteProcessors => this._customRouteProcessors.AsReadOnly();
+
+        public Result<IRouteProcessor> GetRouteForPath(string absolutePath)
+        {
+            foreach (var routeProcessor in this._customRouteProcessors)
+            {
+                if (routeProcessor.IsMatch(absolutePath))
+                {
+                    return Result.Ok(routeProcessor);
+                }
+            }
+
+            foreach (var routeProcessor in this._routeProcessors)
+            {
+                if (routeProcessor.IsMatch(absolutePath))
+                {
+                    return Result.Ok(routeProcessor);
+                }
+            }
+
+            return Result.Fail<IRouteProcessor>();
+        }
+
+        public void AddRouteProcessor(IRouteProcessor routeProcessor)
+        {
+            this._customRouteProcessors.Add(routeProcessor);
+        }
+    }
+}
---
+++ Tests/TrekkingForCharity.AlgoliaLocal.Tests/Infrastructure/RouteProcessorContainerTests.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Trekking for Charity. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System;
+using Moq;
+using TrekkingForCharity.AlgoliaLocal.Infrastructure;
+using TrekkingForCharity.AlgoliaLocal.RouteProcessors;
+using Xunit;
+
+namespace TrekkingForCharity.AlgoliaLocal.Tests.Infrastructure
+{
+    public class RouteProcessorContainerTests
+    {
+        [Fact]
+        public void WhenClassIsInitialized_RouteProcessorsAreDiscoveredCorrectly()
+        {
+            var dataRepository = new Mock<IDataRepository>();
+            var routeProcessorList = new RouteProcessorContainer(dataRepository.Object);
+            Assert.Equal(1, routeProcessorList.RouteProcessors.Count);
+            Assert.Empty(routeProcessorList.CustomRouteProcessors);
+        }
+
+        [Fact]
+        public void GetRouteForPath_WhenRouteIsNotFound_FailedResultIsReturned()
+        {
+            var dataRepository = new Mock<IDataRepository>();
+            var routeProcessorList = new RouteProcessorContainer(dataRepository.Object);
+            var routeResult = routeProcessorList.GetRouteForPath("/");
+            Assert.True(routeResult.IsFailure);
+        }
+
+        [Fact]
+        public void GetRouteForPath_WhenCustomRouteIsFound_RouteIsReturned()
+        {
+            var dataRepository = new Mock<IDataRepository>();
+            var routeProcessorList = new RouteProcessorContainer(dataRepository.Object);
+            var routeProcessor = new Mock<IRouteProcessor>();
+            routeProcessor.Setup(x => x.IsMatch(It.IsAny<string>())).Returns(true);
+            routeProcessorList.AddRouteProcessor(routeProcessor.Object);
+            var routeResult = routeProcessorList.GetRouteForPath("/");
+            Assert.True(routeResult.IsSuccess);
+            Assert.Equal(routeResult.Value, routeProcessor.Object);
+        }
+
+        [Fact]
+        public void GetRouteForPath_WhenRouteIsFound_RouteIsReturned()
+        {
+            var dataRepository = new Mock<IDataRepository>();
+            var routeProcessorList = new RouteProcessorContainer(dataRepository.Object);
+            var routeResult = routeProcessorList.GetRouteForPath("/1/indexes/index");
+            Assert.True(routeResult.IsSuccess);
+            Assert.IsType<IndexRouteProcessor>(routeResult.Value);
+        }
+
+        [Fact]
+        public void GetRouteForPath_WhenCustomRouteAndRouteSharePath_CustomRouteIsReturned()
+        {
+            var dataRepository = new Mock<IDataRepository>();
+            var routeProcessorList = new RouteProcessorContainer(dataRepository.Object);
+            var routeProcessor = new Mock<IRouteProcessor>();
+            routeProcessor.Setup(x => x.IsMatch(It.IsAny<string>())).Returns(true);
+            routeProcessorList.AddRouteProcessor(routeProcessor.Object);
+            var routeResult = routeProcessorList.GetRouteForPath("/1/indexes/index");
+            Assert.True(routeResult.IsSuccess);
+            Assert.Equal(routeResult.Value, routeProcessor.Object);
+        }
+    }
+}
